f41563d67e65dc649009822a43c8003b9a1aa539
MOHAN MUKUND SAI SUNKARA <116540844+SMMS28@users.noreply.github.com>
2024-10-31 14:03:55 +0100

    Add files via upload

---
+++ IIT-Madras-Advanced-Certification-in-Data-Science-and-AI-main/Assignment/Module-Supervised-Learning-Algorithm-In-Python-1/import pyautogui.py
@@ -0,0 +1,8 @@
+import pyautogui 
+import time 
+
+time.sleep(10)
+for i in range(10):
+    pyautogui.typewrite("hi vicky")
+    pyautogui.press("enter")
+    
---
+++ IIT-Madras-Advanced-Certification-in-Data-Science-and-AI-main/README.md
@@ -0,0 +1,2 @@
+# IIT-Madras-Advanced-Certification-in-Data-Science-and-AI
+All the files and datasets related to the course.
